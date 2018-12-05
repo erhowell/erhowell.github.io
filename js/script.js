6c0245a2258b72e55addc612808893ba295f9ec6
erhowell <erhowell15@gmail.com>
2018-12-04 17:58:33 -0700

    Started Adding projects to page
    modals now have a carosel for images of each project

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
 $('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var project = button.data('whatever') 
-  var modal = $(this)
-  var projectData = getDescription(project);
-  modal.find('.modal-title').text(projectData.name)
-  modal.find('.description').text(projectData.description)
-  $(".image").attr("src", projectData.image);
-  $(".link").attr("href",projectData.link);
-});
+  	var button = $(event.relatedTarget) // Button that triggered the modal
+ 	var project = button.data('whatever') 
+  	var modal = $(this)
+  	var projectData = getDescription(project);
+  	modal.find('.modal-title').text(projectData.name)
+  	modal.find('.description').text(projectData.description)
+	for(var i=0 ; i< projectData.image.length ; i++) {
+    $('<div class="carousel-item"><img src="'+projectData.image[i]+'"/></div>').appendTo('.carousel-inner');
+  	}
+  	$('.carousel-item').first().addClass('active');
+  	$('#carouselExampleControls').carousel();
+	$("img").css("width", "100%");
+  	$(".link").attr("href",projectData.link);
+ });
+$('#exampleModal').on('hide.bs.modal', function (event) {
+	$(".carousel-inner").empty();
+ });
 
 function getDescription(project){
-	var projectData= {
-		image: '',
-		name:'',
-		description:'',
-		link:''
-	};
+	console.log('start');
 	switch(project){
 		case 'devSite':
-			projectData.image="./images/portfolioSite.png";
-			projectData.name='Porfolio Site';
-			projectData.description = "Check out the code behind the site!";
-			projectData.link= "https://github.com/erhowell/erhowell.github.io";
+			var projectData= {
+				image:["./images/portfolioSite.png"] ,
+				name:'Porfolio Site',
+				description:'Check out the code behind the site!',
+				link:'https://github.com/erhowell/erhowell.github.io'
+			};
+			return projectData;
+		case 'therapy':
+			var projectData= {
+				image:["./images/therapy/home-page.png", "./images/therapy/login.png",
+				"./images/therapy/your-professional.png", "./images/therapy/find-professional.png"] ,
+				name:'Therapy Site',
+				description:'Check out the code behind the site!',
+				link:'https://github.com/erhowell'
+			};
 			return projectData;
 	}
 }
